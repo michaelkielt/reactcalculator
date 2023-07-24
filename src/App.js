15c92e7eb32ca033aa8630e4c5f6e9cc330655e2
40156896 <michaelkielt@gmail.com>
2023-07-24 16:36:54 +0100

    added css variables as well as other stylings

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ function App() {
           </div>
 
           <div className="digits">
+              { createDigits() }
               <button>0</button>
               <button>.</button>
               <button>=</button>
